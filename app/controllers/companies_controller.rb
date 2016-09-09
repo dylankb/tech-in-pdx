4e434d16d79122571135fdbd121cc4eee39ea94c
dylankb <dylan.barnard@gmail.com>
2016-09-09 14:03:25 -0700

    Users can view all added companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,9 +7,14 @@ class CompaniesController < ApplicationController
     @company = Company.new(company_params)
     if @company.save
       flash[:notice] = "List successfully added!"
+      redirect_to companies_path
     else
       render :new
     end
+  end
+
+  def index
+    @companies = Company.all
   end
 
   def company_params
