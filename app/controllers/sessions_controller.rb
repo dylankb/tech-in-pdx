fb7410598d8fa4d09ed78601977304f56f9bd313
Dylan Barnard <dylan.barnard@gmail.com>
2016-09-16 17:05:32 -0700

    Fix typo on user logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = nil
+    flash[:notice] = "You've logged out"
     redirect_to root_path
   end
 end
