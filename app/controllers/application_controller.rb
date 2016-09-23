182e3a60918e0dca0a04c7c4c873c741c86c1a6b
Dylan Barnard <dylan.barnard@gmail.com>
2016-09-23 10:26:58 -0700

    Fix typo in session exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id] rescue ActiveRecord::RecordNotFound
-     session[:user_id] = nil
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+       session[:user_id] = nil
+    end
   end
   helper_method :current_user
 
