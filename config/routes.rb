9647f415ea954514f1479ff06f4b78468179cfb6
Dylan Barnard <dylan.barnard@gmail.com>
2016-09-23 10:41:52 -0700

    Change how technologies information is displayed for companies In addition to changes to company show and index views, there is now a technology#show view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :companies do
     resources :technologies
   end
+  resources :technologies, :only => [:show]
 
   resources :users
   resources :admin, :only => [:index]
