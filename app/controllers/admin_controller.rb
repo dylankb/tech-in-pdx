55ffb6aba542133abf8c46b8ccca74fa63eb3acc
dylankb <dylan.barnard@gmail.com>
2016-09-28 23:15:12 -0700

    Add editing functionality with AJAX to admin section

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,4 +2,21 @@ class AdminController < ApplicationController
   def index
     @companies = Company.all
   end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      flash[:notice] = @company.name + " updated!"
+      respond_to do |format|
+        format.html { redirect_to admin_path(@company) }
+        format.js
+      end
+    else
+      render :edit
+    end
+  end
 end
