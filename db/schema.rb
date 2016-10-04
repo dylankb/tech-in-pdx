b6d99abb10746a68b5af60be6bb861a0044aac84
Dylan Barnard <dylan.barnard@gmail.com>
2016-10-04 11:38:53 -0700

    Remove unused attribute from companies model, rebundle
    Include missed schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930202059) do
+ActiveRecord::Schema.define(version: 20161004163044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20160930202059) do
   create_table "companies", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.string   "technologies"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "twitter_handle"
