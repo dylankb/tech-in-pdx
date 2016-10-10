58c8273493c8436bcf7885fc2e7334dc553b728f
dylankb <dylan.barnard@gmail.com>
2016-10-10 14:13:52 -0700

    Remove offices table

---
+++ db/migrate/20161010211221_drop_offices_table.rb
@@ -0,0 +1,5 @@
+class DropOfficesTable < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :offices
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006003147) do
+ActiveRecord::Schema.define(version: 20161010211221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,18 +44,6 @@ ActiveRecord::Schema.define(version: 20161006003147) do
   create_table "locations", force: :cascade do |t|
     t.string "city"
     t.string "state"
-  end
-
-  create_table "offices", force: :cascade do |t|
-    t.string   "office_type"
-    t.string   "street_1"
-    t.string   "street_2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
-    t.integer  "company_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "technologies", force: :cascade do |t|
