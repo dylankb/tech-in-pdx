8bfee5126e9492c5d4305349788c2ca5a1ad335e
dylankb <dylan.barnard@gmail.com>
2016-11-09 23:39:24 -0800

    Refactor profile methods to use Enumerable#each_with_object

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -9,7 +9,7 @@ end
 
 
 def github_language_use
-  repositories = github_repositories
+  repositories = github_user_repositories
   language_use_by_repo = repository_languages(repositories)
   total_language_usage = sum_language_usage(language_use_by_repo)
 
@@ -21,41 +21,36 @@ def github_language_use
   end
 end
 
-def github_repositories
+def github_user_repositories
   repos_url = URI("https://api.github.com/users/#{github_username}/repos?access_token=" + ENV['GITHUB_KEY'])
   response = Net::HTTP.get(repos_url)
   parsed_response = JSON.parse(response)
 
-  repo_names = []
-  parsed_response.each do |response|
+  parsed_response.each_with_object([]) do |response, repo_names|
     repo_names << response["name"]
   end
-  repo_names
 end
 
 def repository_languages(repo_names)
-  langs_by_repo = []
-  repo_names.each do |repo|
+  repo_names.each_with_object([]) do |repo, langs_by_repo|
     repo_lang_url = URI("https://api.github.com/repos/#{github_username}/#{repo}/languages?access_token=" + ENV['GITHUB_KEY'])
     response = Net::HTTP.get(repo_lang_url)
     parsed_response = JSON.parse(response)
     langs_by_repo << parsed_response
   end
-  langs_by_repo
 end
 
 def sum_language_usage(langs_by_repo)
-  lang_count = {}
 
-  langs_by_repo.each do |repo|
-    repo.each do |lang_name, count|
-      if lang_count.has_key?(lang_name)
-        lang_count[lang_name] += count
+  lang_use_count = langs_by_repo.each_with_object({}) do |repo, lang_counts|
+    repo.each do |lang_name, bytes|
+      if lang_counts.has_key?(lang_name)
+        lang_counts[lang_name] += bytes
       else
-        lang_count[lang_name] = count
+        lang_counts[lang_name] = bytes
       end
     end
   end
 
-  lang_count.sort_by {|key, value| value }.reverse
+  lang_use_count.sort_by {|key, value| value }.reverse
 end
