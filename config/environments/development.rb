cffb2e060fa3c4a13f9ae157a116e8eadb068783
dylankb <dylan.barnard@gmail.com>
2016-12-08 16:24:11 -0800

    Allow links in Mailer email to be clickable
    Links in an email client, say Gmail, would not display because they did not have localhost as the hostname. Previously, only preview's of Mailer emails were able to be clicked.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
   config.action_mailer.perform_deliveries = true
+  
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
